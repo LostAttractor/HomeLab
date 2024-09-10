d1b739033c6cd2aa0dd984856b11c1c3eeec5cd8
ChaosAttractor <lostattractor@gmail.com>
2024-09-10 15:31:07 +0800

    hardware/ec2: init

---
+++ hardware/ec2/default.nix
@@ -0,0 +1,18 @@
+{ modulesPath, pkgs, lib, ... }:
+{
+  imports = [ (modulesPath + "/virtualisation/amazon-image.nix") ];
+
+  ec2.efi = true;
+
+  # Xanmod Kernel
+  boot.kernelPackages = if (pkgs.system != "aarch64-linux") then lib.mkDefault pkgs.linuxPackages_xanmod_latest else lib.mkDefault pkgs.linuxPackages_latest;
+
+  # Allow the user to login as root without password.
+  users.users.root.initialHashedPassword = lib.mkOverride 150 "";
+
+  # Some more help text.
+  services.getty.helpLine = ''
+
+    Log in as "root" with an empty password.
+  '';
+}
