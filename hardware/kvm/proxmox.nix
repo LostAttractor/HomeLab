89e57ba163190201b525961459102d61a2ca4700
ChaosAttractor <lostattractor@gmail.com>
2024-10-13 18:47:08 +0800

    hardware: use fstrim

--- hardware/kvm/proxmox.nix
+++ hardware/kvm/proxmox.nix
@@ -33,6 +33,8 @@
     size = 4*1024;
   } ];
 
+  services.fstrim.enable = true;
+
   # Allow the user to login as root without password.
   users.users.root.initialHashedPassword = lib.mkOverride 150 "";
 
