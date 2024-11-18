0eda84a7c3c08ac9e9776e9d858efbac2050a15b
ChaosAttractor <lostattractor@gmail.com>
2024-11-18 18:06:08 +0800

    features/nix: add trusted-public-keys

--- features/nix/default.nix
+++ features/nix/default.nix
@@ -12,4 +12,7 @@
   # Free up to 1GiB whenever there is less than 100MiB left
   nix.settings.min-free = lib.mkDefault "${toString (100 * 1024 * 1024)}";
   nix.settings.max-free = lib.mkDefault "${toString (1024 * 1024 * 1024)}";
+
+  # Trusted Public Keys
+  nix.settings.trusted-public-keys =  [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
 }
