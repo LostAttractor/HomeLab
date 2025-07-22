aa75ba6f73555adc5a53a6ed484957a52c5d8948
ChaosAttractor <lostattractor@gmail.com>
2025-07-22 11:20:47 +0800

    featrues/nix: add hydra keys

--- features/nix/default.nix
+++ features/nix/default.nix
@@ -14,5 +14,8 @@
   nix.settings.max-free = lib.mkDefault "${toString (1024 * 1024 * 1024)}";
 
   # Trusted Public Keys
-  nix.settings.trusted-public-keys =  [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
+  nix.settings.trusted-public-keys =  [
+    "hydra:bCXoAKNbKou4hrnzaH2YB0nvbBPj81PmTklSUSQr5I8="
+    "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc="
+  ];
 }
